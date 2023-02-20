a25b50b74792726431d8a1ef126c5eb673d5e5f4
Huub Bouma <huub@pythonunited.com>
2023-02-20 14:41:52 +0100

    ContentCopied event + IObjectCopiedEvent interface added

--- w20e/hitman/events.py
+++ w20e/hitman/events.py
@@ -5,7 +5,7 @@ from zope.interface import implementer, Attribute
 
 class ObjectEvent(object):
 
-    """ Initialize event """
+    """Initialize event"""
 
     def __init__(self, obj, parent):
 
@@ -15,41 +15,59 @@ class ObjectEvent(object):
 
 class IObjectAddedEvent(IObjectEvent):
 
-    """ Interface for added objects """
+    """Interface for added objects"""
 
-    object = Attribute('The object being added')
-    parent = Attribute('The folder to which the object is being added')
+    object = Attribute("The object being added")
+    parent = Attribute("The folder to which the object is being added")
 
 
 class IObjectRemovedEvent(IObjectEvent):
 
-    """ Interface for removed objects """
+    """Interface for removed objects"""
 
-    object = Attribute('The object being added')
-    parent = Attribute('The folder from which the object is removed')
+    object = Attribute("The object being added")
+    parent = Attribute("The folder from which the object is removed")
 
 
 class IObjectChangedEvent(IObjectEvent):
 
-    """ Interface for changed objects """
+    """Interface for changed objects"""
+
+
+class IObjectCopiedEvent(IObjectEvent):
+
+    """Interface for copied objects"""
+
+    object = Attribute("The new object that was added")
+    source = Attribute("The original object")
 
 
 @implementer(IObjectAddedEvent)
 class ContentAdded(ObjectEvent):
 
-    """ Object is added """
+    """Object is added"""
 
 
 @implementer(IObjectRemovedEvent)
 class ContentRemoved(ObjectEvent):
 
-    """ Object is removed """
+    """Object is removed"""
 
 
 @implementer(IObjectChangedEvent)
 class ContentChanged(ObjectEvent):
 
-    """ Object is changed """
+    """Object is changed"""
 
     def __init__(self, object):
         self.object = object
+
+
+@implementer(IObjectCopiedEvent)
+class ContentCopied(ObjectEvent):
+
+    """Object is copied"""
+
+    def __init__(self, object, source):
+        self.object = object
+        self.source = source
